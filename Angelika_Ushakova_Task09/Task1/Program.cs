f9e312f0e4759b53c1dbac342d590e9d094089d1
Angelika <lika_ushakova@mail.ru>
2018-10-23 23:52:28 +0300

    Task09

---
+++ Angelika_Ushakova_Task09/Task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static T RemoveEachSecondItem<T>(IEnumerable<T> list)
+        {
+            byte secondindex = 0;
+
+            if (list.Count() == 0)
+            {
+                throw new Exception("Список пуст");
+            }
+
+            while (list.Count() > 1)
+            {
+                list = list.Where(i => secondindex++ % 2 == 0).ToList();
+            }
+            return list.First();
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() {1,2,3,4,5,6};
+            List<int> linklist = new List<int>() { 1, 4, 3, 4, 8, 2, 4, 9 };
+
+            LinkedList<int> linkedList = new LinkedList<int>(linklist);
+
+            Console.WriteLine(RemoveEachSecondItem(list));
+            Console.WriteLine(RemoveEachSecondItem(linkedList));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Angelika_Ushakova_Task09/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> wordsSet = new HashSet<string>();
+            string text = "Hi, how are you. are you are";
+           
+            string[] words = Regex.Split(text, @"[\W]+");
+            for(int i = 0; i < words.Length; i++)
+            {
+                wordsSet.Add(words[i].ToLower());
+            }
+
+            List<KeyValuePair<string, float>> wordsCount = new List<KeyValuePair<string, float>>();
+            foreach ( string word in wordsSet)
+            {
+                wordsCount.Add(new KeyValuePair<string, float>(word, Regex.Split(text.ToLower(), word).Count()-1)); 
+            }
+            for (int i = 0; i < wordsCount.Count; i++)
+            {
+                Console.WriteLine($"{wordsCount[i].Key}  { wordsCount[i].Value / words.Count():0.00}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
